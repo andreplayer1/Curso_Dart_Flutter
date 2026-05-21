98a06a16b01a338d4e353be5dba07c85566c0333
André Barbosa <123273445+andreplayer1@users.noreply.github.com>
2026-05-21 05:06:14 -0300

    feat(poo)
    implementa encapsulamento e interacao entre objetos no combate

--- Aulas_Praticas/53Construtores e Construtores Nomeados.dart
+++ Aulas_Praticas/53Construtores e Construtores Nomeados.dart
@@ -1,19 +1,27 @@
-void main (){
-  Personagem personagem1 = Personagem(nome: 'Kratos', classeRPG: 'Assassino', nivel: 100, pontosDeVida: 100);
-  Personagem personagem2 = Personagem.novato(nome: 'Kiara', classeRPG: 'Mago');
-  print(personagem2.nome);
-  print(personagem2.classeRPG);
-  print(personagem2.pontosDeVida);
-  personagem2.atacar();
+void main() {
+  Personagem personagem1 = Personagem(
+    nome: 'Kratos',
+    classeRPG: 'Assassino',
+    nivel: 50,
+    pontosDeVida: 2000,
+  );
+  Personagem personagem2 = Personagem.novato(
+    nome: 'Kiara', 
+    classeRPG: 'Mago', 
+    nivel: 25);
+  personagem2.atacar(personagem1);
 }
 
 class Personagem {
 
-  Personagem({required this.nome, required this.classeRPG, required this.nivel, required this.pontosDeVida});
+  Personagem({required this.nome,required this.classeRPG,required this.nivel,required this.pontosDeVida,}) {
+    print('CRIANDO PERSONAGEM: \n$nome \n$classeRPG \nNv. $nivel \nVida: $pontosDeVida');
+    print('********************');
+  }
 
-  Personagem.novato({required this.nome, required this.classeRPG}){
-    pontosDeVida = 100;
-    nivel = 1;
+  Personagem.novato({required this.nome, required this.classeRPG, this.pontosDeVida = 100, this.nivel = 1}) {
+    print('CRIANDO PERSONAGEM: \n$nome \n$classeRPG \nNv. $nivel \nVida: $pontosDeVida');
+    print('********************');
   }
 
   String nome;
@@ -21,17 +29,20 @@ class Personagem {
   int nivel = 0;
   double pontosDeVida = 0;
 
-  void atacar(){
-    if(classeRPG == 'Mago'){
+  void atacar(Personagem alvo) {
+    if (classeRPG == 'Mago') {
       double dano = 50.0 + nivel;
-    print('O personagem $nome atacou com força de nivel $dano');
+      print('O personagem $nome atacou o oponente ${alvo.nome} com força de nivel $dano');
+      alvo.sofrerDano(dano);
     } else {
       double dano = 0.0 + nivel;
-      print('O personagem $nome atacou com força de nivel $dano');
-      }
+      print('O personagem $nome atacou o oponente ${alvo.nome} com força de nivel $dano');
+      alvo.sofrerDano(dano);
+    }
   }
 
-  void sofrerDano(double dano){
-
+  void sofrerDano(double dano) {
+    pontosDeVida -= dano;
+    print('$nome sofreu um ataque \nVida: $pontosDeVida');
   }
 }
