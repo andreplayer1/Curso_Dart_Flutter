f1b12f2c01a3b5f4eb8a005d196c02daa8fcffd3
André Barbosa <123273445+andreplayer1@users.noreply.github.com>
2026-05-24 19:25:22 -0300

    refactor(poo): implementa encapsulamento com getters e setters na classe Personagem
    - Transforma atributos nivel e pontosDeVida em privados.
    
    - Adiciona regras de validacao nos Setters.
    
    - Atualiza construtores para utilizar as portas de acesso seguras.

--- Aulas_Praticas/53Construtores e Construtores Nomeados.dart
+++ Aulas_Praticas/53Construtores e Construtores Nomeados.dart
@@ -6,43 +6,81 @@ void main() {
     pontosDeVida: 2000,
   );
   Personagem personagem2 = Personagem.novato(
-    nome: 'Kiara', 
-    classeRPG: 'Mago', 
-    nivel: 25);
+    nome: 'Kiara',
+    classeRPG: 'Mago'
+  );
   personagem2.atacar(personagem1);
 }
 
 class Personagem {
 
-  Personagem({required this.nome,required this.classeRPG,required this.nivel,required this.pontosDeVida,}) {
-    print('CRIANDO PERSONAGEM: \n$nome \n$classeRPG \nNv. $nivel \nVida: $pontosDeVida');
+  Personagem({required this.nome, required this.classeRPG, required int nivel, required double pontosDeVida})
+  {
+  this.nivel = nivel;
+  this.pontosDeVida = pontosDeVida;
+
+    print('CRIANDO PERSONAGEM: \n$nome \n$classeRPG \nNv. $_nivel \nVida: $_pontosDeVida',);
     print('********************');
   }
 
-  Personagem.novato({required this.nome, required this.classeRPG, this.pontosDeVida = 100, this.nivel = 1}) {
-    print('CRIANDO PERSONAGEM: \n$nome \n$classeRPG \nNv. $nivel \nVida: $pontosDeVida');
+  Personagem.novato({required this.nome, required this.classeRPG,})
+  {
+    this.nivel = 1;
+    this.pontosDeVida = 100.0;
+
+    print(
+      'CRIANDO PERSONAGEM: \n$nome \n$classeRPG \nNv. $_nivel \nVida: $_pontosDeVida',
+    );
     print('********************');
   }
 
   String nome;
   String classeRPG;
-  int nivel = 0;
-  double pontosDeVida = 0;
+  int _nivel = 0;
+  double _pontosDeVida = 0.0;
 
   void atacar(Personagem alvo) {
     if (classeRPG == 'Mago') {
-      double dano = 50.0 + nivel;
-      print('O personagem $nome atacou o oponente ${alvo.nome} com força de nivel $dano');
+      double dano = 50.0 + _nivel;
+      print(
+        'O personagem $nome atacou o oponente ${alvo.nome} com força de nivel $dano',
+      );
       alvo.sofrerDano(dano);
     } else {
-      double dano = 0.0 + nivel;
-      print('O personagem $nome atacou o oponente ${alvo.nome} com força de nivel $dano');
+      double dano = 0.0 + _nivel;
+      print(
+        'O personagem $nome atacou o oponente ${alvo.nome} com força de nivel $dano',
+      );
       alvo.sofrerDano(dano);
     }
   }
 
   void sofrerDano(double dano) {
-    pontosDeVida -= dano;
-    print('$nome sofreu um ataque \nVida: $pontosDeVida');
+    _pontosDeVida -= dano;
+    print('$nome sofreu um ataque \nVida: $_pontosDeVida');
+  }
+
+  double get pontosDeVida {
+    return _pontosDeVida;
+  }
+
+  set pontosDeVida(double valor) {
+    if (valor >= 0 && valor <= 9999) {
+      _pontosDeVida = valor;
+    } else {
+      _pontosDeVida = 0;
+    }
+  } 
+
+  int get nivel {
+    return _nivel;
+  }
+
+  set nivel(int valorAtual) {
+    if (valorAtual >= 1 && valorAtual <= 99) {
+      _nivel = valorAtual;
+    } else {
+      _nivel = 1;
+    }
   }
 }
